87298137dba889a0b7f41c9aca001adb1eb93d7a
Annalisa28 <120007375+Annalisa28@users.noreply.github.com>
2023-03-09 13:10:28 +0100

    esercizio 3 svolto

--- index.js
+++ index.js
@@ -2,5 +2,33 @@
 import './style.css';
 
 // Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+let libriTrovati=[];
+class Archivio{
+  constructor(autore,titolo){
+    this.autore=autore;
+    this.titolo=titolo;
+  }
+  trovaLibri(stringa){
+    if(
+      this.autore.toLowerCase().includes(stringa.toLowerCase()) || this.titolo.toLowerCase().includes(stringa.toLowerCase())
+      )
+      {
+      libriTrovati.push({autore:this.autore, titolo:this.titolo});
+    }
+  }
+}
+
+const AutoriTitoli=[];
+let stringa= 'da';
+
+AutoriTitoli[AutoriTitoli.length]=new Archivio ("Dante Alighieri","La Divina Commedia");
+AutoriTitoli[AutoriTitoli.length]=new Archivio ("Italo Svevo","Senilità");
+AutoriTitoli[AutoriTitoli.length]=new Archivio ("Pirandello","Il fu Mattia Pascal");
+AutoriTitoli[AutoriTitoli.length]=new Archivio ("Manzoni","I promessi sposi");
+
+AutoriTitoli.forEach((singoloLibro) => singoloLibro.trovaLibri(stringa));
+
+libriTrovati.forEach((singoloLibro)=> console.log(singoloLibro["titolo"] + singoloLibro["autore"]));
+
+console.log("ciao");
+
